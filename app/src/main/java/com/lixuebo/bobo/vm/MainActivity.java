a58b9335e89891c398c03468b1e97dd8eb66fa25
lixuebo <18511899797@163.com>
2017-03-02 17:14:46 +0800

    创建项目.

---
+++ app/src/main/java/com/lixuebo/bobo/vm/MainActivity.java
@@ -0,0 +1,16 @@
+package com.lixuebo.bobo.vm;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.lixuebo.bobo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
